885fbd80f30176864cae8e2adb95ff9d1b6c6b18
João Vitor Gomes Pontes <joao.pontes29@etec.sp.gov.br>
2021-06-01 11:47:25 -0300

    01/06/2021

---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,20 @@
+import { RestaurantsService } from './restaurants.service';
+import { Restaurant } from './restaurant/restaurant.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants',
+  templateUrl: './restaurants.component.html',
+  styleUrls: ['./restaurants.component.css']
+})
+export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[];
+
+  constructor(private restaurantsService: RestaurantsService) { }
+
+  ngOnInit(): void {
+    this.restaurants=this.restaurantsService.restaurants();
+  }
+
+}
