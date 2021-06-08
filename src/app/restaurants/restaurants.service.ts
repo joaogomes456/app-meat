2e4f1558f50511c53dce593ee9643aee63338bf4
João Vitor Gomes Pontes <joao.pontes29@etec.sp.gov.br>
2021-06-08 12:33:31 -0300

    08/06/2021
    Comunicação com o banco de dados emulado feita.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,35 +1,16 @@
 import { Restaurant } from './restaurant/restaurant.model';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantsService {
 
-  rests: Restaurant[] = [
-    {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Padaria",
-      deliveryEstimate: "25m",
-      rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png"
-    },
+  constructor(private http: HttpClient) {}
 
-    {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgueria",
-      deliveryEstimate: "100m",
-      rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png"
-    }
-
-  ];
-
-  constructor() {}
-
-  restaurants(): Restaurant[] {
-    return this.rests;
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get<Restaurant[]>(`http://localhost:3000/restaurants`);
   }
 }
