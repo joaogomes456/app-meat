fce9fd53b487c5865dee764624b170dc77661949
JoãoGomes456 <joao.pontes29@etec.sp.gov.br>
2021-09-18 08:59:59 -0300

    18/09/2021
    Pagina restaurant-detail adicionada.
    OBS: A ser continuada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RestaurantsService } from './restaurants/restaurants.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ToastrModule } from 'ngx-toastr';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { ToastrModule } from 'ngx-toastr';
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent
   ],
   imports: [
     BrowserModule,
